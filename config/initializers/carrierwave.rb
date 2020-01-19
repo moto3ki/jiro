de0ce888bc80a3eabaffcde9b671b3d94eb47e2b
moto3ki <moto3ki@gmail.com>
2020-01-19 22:50:59 +0900

    s3_setting

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,22 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+# 開発環境ではローカルに保存
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+# 本番はS3に保存
+  elsif Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id:  ENV["G_AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["G_AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1'
+  }
+  config.fog_directory  = 'jiro63'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/jiro63'
+  end
+end
