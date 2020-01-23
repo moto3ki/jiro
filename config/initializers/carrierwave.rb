4d3ee9467903d1f0fdfcd825f45a6f1418c68c15
moto3ki <moto3ki@gmail.com>
2020-01-23 14:30:23 +0900

    S3_bakket_resetting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,7 +16,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: 'ap-northeast-1'
   }
-  config.fog_directory  = 'jiro63'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/jiro63'
+  config.fog_directory  = 'jiro63b'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/jiro63b'
   end
 end
